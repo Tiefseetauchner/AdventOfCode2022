b47fb9b3ab2d0155ea6445430c2e931cce613af1
tauchner <lena@tauchner.me>
2022-12-01 23:42:46 +0100

    Completed Day 1

---
+++ day01.nim
@@ -0,0 +1,20 @@
+import nre
+import algorithm
+import sequtils
+import aochelpers
+
+const input = readFile("inputs/day01-01.txt")
+
+var elves = input.split(re"\n\n")
+
+echo elves.len
+
+sort(elves, cmpLines)
+
+var elveCals = map(elves, addLines)
+
+sort(elveCals, SortOrder.Descending)
+
+echo "Question 1: ", elveCals[0]
+
+echo "Question 2: ", (elveCals[0] + elveCals[1] + elveCals[2])
