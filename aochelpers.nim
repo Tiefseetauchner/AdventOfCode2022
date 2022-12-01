6bdd6713a24ec27aa1f80dc455708026bd177116
tauchner <lena@tauchner.me>
2022-12-01 23:46:59 +0100

    Removed more stupid

--- aochelpers.nim
+++ aochelpers.nim
@@ -1,15 +1,10 @@
 import strutils
 import nre
 
-proc cmpLines*(x, y: string): int =
-  cmp(y.count("\n"), x.count("\n"))
-
 proc addLines*(lines: string): int =
   let nums = lines.split(re"\n")
 
   var sum = 0;
-
   for num in nums:
     sum += parseInt(num)
-
   sum
